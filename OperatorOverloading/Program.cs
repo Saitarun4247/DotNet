26fa22a87177686ab3f5951511e5ecc09ab1e6e5
Sai Tharun <117085454+Saitarun4247@users.noreply.github.com>
2026-01-07 11:30:33 +0530

    Add files via upload

---
+++ OperatorOverloading/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+int num1=100;
+int num2=200;
+
+int numResult=num1+num2;
+
+System.Console.WriteLine(numResult);
+
+Employee emp1=new Employee();
+emp1.EmpID=101;
+emp1.EmpName="Alok";
+emp1.BSalary=40000;
+
+Employee emp2=new Employee();
+emp2.EmpID=102;
+emp2.EmpName="Riya";
+emp2.BSalary=35000;
+
+Employee emp3=new Employee();
+emp3.EmpID=1798;
+emp3.EmpName="Manjeet";
+emp3.BSalary=65000;
+
+// int numResult = num1+num2;
+Employee empObj = emp1+emp2+emp3;
+
+MyLogic m1= new MyLogic();
+m1.
+
+System.Console.WriteLine( $" Total Sal Paid {empObj.BSalary}");
+
